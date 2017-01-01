80ed5c0712d487f6dd86040c4b008b704f8ad7fc
litengsz <1773098@qq.com>
2017-01-01 08:16:14 +0800

    Initial Commit

---
+++ gittest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gittest
+//
+//  Created by Liuliteng on 2017/1/1.
+//  Copyright © 2017年 Liuliteng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
